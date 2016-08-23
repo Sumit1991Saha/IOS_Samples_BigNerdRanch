fc7024c4938fee5caf280a989f6f0f18f47b2a20
Sumit Saha <kumsaha@gmail.com>
2016-08-23 14:10:08 +0530

    Add files via upload

---
+++ Chapter%205/Hypnosister/Hypnosister/HypnosisView.h
@@ -0,0 +1,13 @@
+//
+//  HypnosisView.h
+//  Hypnosister
+//
+//  Created by Sumit Saha on 22/08/16.
+//  Copyright © 2016 Sumit Saha. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HypnosisView : UIView
+
+@end
